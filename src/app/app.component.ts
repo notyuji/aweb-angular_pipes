55fd609689fdb1b7bae840977d9a2e893e03f383
Yuji <mallarilevin@gmail.com>
2025-01-19 23:41:26 +0800

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Angular Pipes Demo';
 
-  text = 'hello world';
+  text = 'Welcome';
   date = new Date();
   price = 1234.56;
   decimalValue = 3.14159;
-  jsonData = { name: 'John', age: 25, address: '123 Main Street' };
-  items = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+  jsonData = { name: 'Levin Mallari', age: 20, address: 'Babo Pangulo, Porac, Pampanga' };
+  items = ['Plastic', 'Bag', 'Car', 'Pencil', 'Mouse'];
 }
